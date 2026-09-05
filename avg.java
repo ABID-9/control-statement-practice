2b7714180bff7206a5e786bb2cdc2163507a0534
ABID-9 <abidulislam2828@gmail.com>
2026-09-06 00:01:06 +0600

    Updated Java code

---
+++ avg.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class avg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a : ") ;
+        double a = sc.nextInt();
+        System.out.println("enter b : ");
+        double b = sc.nextInt();
+        System.out.println("enter c :");
+        double c = sc.nextInt();
+        double average = (a+b+c)/3;
+        System.out.println("ans is "+average);
+        System.out.println("abid");
+
+    }
+    
+}
